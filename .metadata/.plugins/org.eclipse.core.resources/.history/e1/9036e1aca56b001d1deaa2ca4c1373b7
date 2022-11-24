e8eb6e09c22bdc1318bbf1797a1be77eb6122269
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2022-11-24 08:41:41 +0530

    MaximumArraySumAfterB

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e1/9036e1aca56b001d1deaa2ca4c1373b7
@@ -0,0 +1,10 @@
+package com.scaler.dsa.heap.assignment;
+
+public class MaximumArraySumAfterB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- DSA/src/com/scaler/dsa/heap/assignment/MaximumArraySumAfterB.java
+++ DSA/src/com/scaler/dsa/heap/assignment/MaximumArraySumAfterB.java
@@ -1,6 +1,34 @@
 package com.scaler.dsa.heap.assignment;
 
+import java.util.PriorityQueue;
+
 public class MaximumArraySumAfterB {
+	
+    public int solve(int[] a, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int x : a)
+            pq.add(x);
+ 
+        // Do k negations by removing a minimum element k times
+        while (k-- > 0)
+        {
+            // Retrieve and remove min element
+            int temp = pq.poll();
+ 
+            // Modify the minimum element and add back
+            // to priority queue
+            temp *= -1;
+            pq.add(temp);
+        }
+ 
+        // Compute sum of all elements in priority queue.
+        int sum = 0;
+        for (int x : pq)
+            sum += x;
+            
+        return sum;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
